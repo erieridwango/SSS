0c7ffd03ccaf5bf97d7727e78a0bdc895b880691
erieridwango <125840485+erieridwango@users.noreply.github.com>
2023-08-15 14:44:34 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -39,6 +39,9 @@ assessmentButton.onclick =  () => {
  anchor.innerText = 'Tweet #あなたのいいところ';
 
  tweetDivision.appendChild(anchor);
+ const script = document.createElement('script');
+     script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+     tweetDivision.appendChild(script)
 };
 
 
